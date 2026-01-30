1a5c98b827617c4d0e44f4d6af8ad0d3391e925e
Dikara Derasetiana <dikaraderandia.jtk10@gmail.com>
2026-01-30 23:07:44 +0700

    Validation login (#3)
    * feat(user-service): add JWT authentication token signature from auth service
    
    * feat(user-service): add handler when got exception expred token, token invalid, and access denied
    
    * feat(user-service): integrating user-service and auth-service using feign eureka
    
    * feat(user-service): fixing scan CustomResponseAdvise packages user to auth
    
    * feat(user-service): add Dockerfile, update pom, configure JWT replace validation
    
    * feat(user-service): implement swagger and adjustement for feign from auth service
    
    ---------
    
    Co-authored-by: Dikara Derandia <dikara.derandia@dwidasa.com>

--- src/main/java/com/dikara/user/controller/UserController.java
+++ src/main/java/com/dikara/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.dikara.user.dto.response.PaginationResponse;
 import com.dikara.user.dto.response.UserResponse;
 import com.dikara.user.service.UserService;
 import com.dikara.user.util.JwtPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,11 +18,13 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User", description = "User management API")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Get all users")
     @GetMapping("/getAll")
    // @PreAuthorize("hasRole('ADMIN')")
     public List<UserResponse> findAll(  @RequestHeader("X-User-Id") String userId,
@@ -37,22 +41,26 @@ public class UserController {
         return "SHOULD NEVER SHOW";
     }
 
+    @Operation(summary = "Create users")
     @PostMapping
     public UserResponse create(@RequestBody UserRequest user){
         return userService.createUser(user);
 
     }
 
+    @Operation(summary = "Delete user by Id")
     @DeleteMapping ("/{id}")
     public Map<String, String> delete (@PathVariable String id){
         return userService.deleteUser(id);
     }
 
+    @Operation(summary = "Update user by Id")
     @PutMapping("/{id}")
     public UserResponse update (@PathVariable String id,@RequestBody UserRequest user){
         return userService.updateUser(id, user);
     }
    // @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Get all users with pagination format")
     @GetMapping("/pagination")
     public PaginationResponse<UserResponse> findAllUsersWithPagination(
             @RequestParam(defaultValue = "1") int page,
@@ -61,10 +69,11 @@ public class UserController {
         return userService.findAll(page, size, keyword);
     }
 
+    @Operation(summary = "Get all users with pagination format")
     @GetMapping("/me")
-    public UserResponse getCurrentUser(@AuthenticationPrincipal JwtPrincipal principal) {
+    public UserResponse getCurrentUser(@RequestHeader("X-User-Id") String userId) {
 
-        UserResponse result = userService.findById(principal.getUserId());
+        UserResponse result = userService.findById(userId);
         return result;
     }
 }
